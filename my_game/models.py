4ef37aa3e55975c7d81b63f76fd3c86c2f24f42f
VicHunting <VicHunting@yandex.ua>
2018-05-07 01:36:23 +0300

    Extend set_new_location method in Gamer model

--- my_game/models.py
+++ my_game/models.py
@@ -1,3 +1,4 @@
+from typing import Union
 from random import randrange
 
 from django.db import models
@@ -61,7 +62,7 @@ class Gamer(models.Model):
         unique_together = ('user', 'location_x', 'location_y')
 
     def __str__(self):
-        return "{username} on (x, y)".format(
+        return "{username} on ({x}, {y})".format(
             username=self.user.username,
             x=self.location_x,
             y=self.location_y,
@@ -74,7 +75,7 @@ class Gamer(models.Model):
                 super().save(*args, **kwargs)
                 break
             except IntegrityError:
-                self._new_location()
+                self._new_rand_location()
 
     @classmethod
     def from_user(cls, user):
@@ -83,19 +84,29 @@ class Gamer(models.Model):
         """
         return Gamer.objects.get(user=user)
 
-    def set_new_location(self, save=True):
+    def set_new_location(self,
+                         x: Union[int, None]=None,
+                         y: Union[int, None]=None,
+                         save: bool=True):
         """
         Set new location for the gamer on the GameGrid.
+
+        If x and y is not provided, random location will be set.
 
         :return: new coordinates (x, y)
         """
 
-        self._new_location()
+        if x is None and y is None:
+            self._new_rand_location()
+
+        else:
+            self.location_x = x
+            self.location_y = y
 
         if save is True:
             self.save()
 
         return self.location_x, self.location_y
 
-    def _new_location(self):
+    def _new_rand_location(self):
         self.location_x, self.location_y = GameGrid.random_location()
