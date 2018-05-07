fce47360bfd2f12f28c8084ffd78b56b059964ab
VicHunting <VicHunting@yandex.ua>
2018-05-07 06:39:44 +0300

    Fix error with dir paths in settings

--- cell_gamers/settings.py
+++ cell_gamers/settings.py
@@ -12,9 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_DIR = os.path.dirname(BASE_DIR)
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PARENT_DIR = os.path.dirname(PROJECT_DIR)
 
 
